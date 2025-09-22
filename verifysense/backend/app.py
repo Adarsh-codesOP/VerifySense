cb18ff34022243c1cb61ac7d4ca3487b584cf813
AdarshAS007 <adarshas107@gmail.com>
2025-09-22 19:48:14 +0530

    Update app.py

--- verifysense/backend/app.py
+++ verifysense/backend/app.py
@@ -79,4 +79,4 @@ def submit_feedback():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5050))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
